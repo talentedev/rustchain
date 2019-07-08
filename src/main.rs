8a58b6d384d055f62c4b4de0cf8eda988a0cf428
talentedev <webdev124@outlook.com>
2019-07-08 10:38:00 +0100

    refactor get input function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate time;
 extern crate sha1;
 extern crate bincode;
+extern crate termion;
 
 extern crate serde;
 #[macro_use] extern crate serde_derive;
@@ -18,6 +19,11 @@ use bincode::{
     serialize,
     deserialize,
 };
+use termion::{
+    color,
+    terminal_size,
+};
+use termion::cursor::Goto;
 
 #[derive(Serialize, Deserialize)]
 struct HashContent {
@@ -118,25 +124,38 @@ impl Block {
 
 /// Handles user input and returns that input as a string.
 ///
+/// Args:
+///
+/// `height` - the terminal height
+///
 /// Returns:
 ///
 /// user input as string
-fn get_input() -> String {
+fn get_input(height: u16) -> String {
+
+    println!("{}", Goto(0, height - 3));
 
     let mut input = String::new();
     stdin().read_line(&mut input).expect("cannot read input");
 
-    input
+    clear_screen(height);
+    println!("{}", Goto(0, 2));
+
+    input.trim().to_string()
 }
 
 /// Returns an address:port string from the user input. Refactored as used multiple times.
 ///
+/// Args:
+///
+/// `height` - the terminal height
+///
 /// Returns:
 ///
 /// bind address in "address:port" format
-fn get_bind_address_from_input() -> String {
+fn get_bind_address_from_input(height: u16) -> String {
 
-    let input = get_input();
+    let input = get_input(height);
     let address = input.trim();
 
     const PORT: &str = "10000";
@@ -148,34 +167,71 @@ fn get_bind_address_from_input() -> String {
     ).to_string()
 }
 
+/// Display text into a blue bar with a width that is as long as the terminal width. Refactored as it is used multiple times.
+///
+/// Args:
+///
+/// `text` - the text to display into the text bar
+fn display_text_bar(text: &str) {
+
+    println!(
+        "{}{}{}{}{}{}",
+        color::Bg(color::Blue),
+        color::Fg(color::White),
+        text,
+        std::iter::repeat(' ')
+            .take(terminal_size().unwrap().0 as usize - text.len())
+            .collect::<String>(),
+        color::Bg(color::Reset),
+        color::Fg(color::Reset),
+    );
+}
+
+/// Clear the whole terminal content and generate the default content (bars and titles). Refactored as used multiple times and definition might not be clear.
+///
+/// Args:
+///
+/// `height` - the terminal height
+fn clear_screen(height: u16) {
+
+    /* send a control character to the terminal */
+    print!("{}[2J", 27 as char);
+
+    println!("{}", Goto(1, 1));
+    display_text_bar("rust-blockchain");
+
+    println!("{}", Goto(0, height - 1));
+    display_text_bar("Waiting. Type 'help' to get the commands list.");
+}
+
 fn main() {
+
+    let (_, height) = terminal_size().unwrap();
+    let height = height as u16;
+
+    clear_screen(height);
 
     let genesis = Block::new(0, String::new());
     let mut chain: Vec<Block> = vec![genesis];
 
+    println!("{}", Goto(0, 2));
     println!("Genesis block has been generated.");
 
     loop {
 
-        println!("\nChoices:");
-        println!("1. Add a block");
-        println!("2. Send blockchain");
-        println!("3. Receive blockchain");
-        println!("4. See local blockchain");
+        let input = get_input(height);
 
-        let input = get_input();
-        let choice = input.as_bytes()[0];
+        const ADD_BLOCK_CHOICE: &str = "add_block";
+        const SEND_BLOCKCHAIN_CHOICE: &str = "send";
+        const RECEIVE_BLOCKCHAIN_CHOICE: &str = "receive";
+        const SEE_BLOCKCHAIN_CHOICE: &str = "list";
+        const HELP_CHOICE: &str = "help";
 
-        const ADD_BLOCK_CHOICE: u8 = 0x31;
-        const SEND_BLOCKCHAIN_CHOICE: u8 = 0x32;
-        const RECEIVE_BLOCKCHAIN_CHOICE: u8 = 0x33;
-        const SEE_BLOCKCHAIN_CHOICE: u8 = 0x34;
-
-        if choice == ADD_BLOCK_CHOICE {
+        if input == ADD_BLOCK_CHOICE {
 
             println!("Data of the block:");
 
-            let input = get_input();
+            let input = get_input(height);
             let data: i32 = input.trim().parse().unwrap();
 
             let current_digest = chain.last()
@@ -190,30 +246,30 @@ fn main() {
 
             chain.push(block);
         }
-        else if choice == SEND_BLOCKCHAIN_CHOICE {
+        else if input == SEND_BLOCKCHAIN_CHOICE {
 
             println!("Send blockchain to node at IP:");
 
-            let bind_address = get_bind_address_from_input();
+            let bind_address = get_bind_address_from_input(height);
             let mut stream = TcpStream::connect(bind_address).unwrap();
 
             let bytes = serialize(&chain).unwrap();
-            stream.write(&bytes);
+            stream.write(&bytes).unwrap();
         }
-        else if choice == RECEIVE_BLOCKCHAIN_CHOICE {
+        else if input == RECEIVE_BLOCKCHAIN_CHOICE {
 
             let listener = TcpListener::bind("0.0.0.0:10000").unwrap();
 
             println!("Waiting for connection...");
 
-            let mut connection = listener.accept().unwrap();
+            let connection = listener.accept().unwrap();
 
             println!("Connection received.");
 
             let mut buffer: Vec<u8> = Vec::new();
             let mut stream = connection.0;
 
-            stream.read_to_end(&mut buffer);
+            stream.read_to_end(&mut buffer).unwrap();
 
             /* TODO: check integrity of the received chain */
 
@@ -222,7 +278,7 @@ fn main() {
                 chain = received_chain;
             }
         }
-        else if choice == SEE_BLOCKCHAIN_CHOICE {
+        else if input == SEE_BLOCKCHAIN_CHOICE {
 
             for block in chain.iter() {
 
@@ -231,6 +287,15 @@ fn main() {
                 println!("Timestamp: {}", content.get_timestamp());
                 println!("Data: {} \n\n", content.get_data());
             }
+        }
+        else if input == HELP_CHOICE {
+
+            /* TODO: should use command options */
+
+            println!("add_block - append a block into the local blockchain");
+            println!("send - send a copy of the blockchain to another node");
+            println!("receive - receive a copy of the blockchain from another node");
+            println!("list - list the local chain blocks");
         }
     }
 }
